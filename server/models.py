20677c6c90a2a6852017471b18555f47e1dd42ac
Artem Raskin <artemraskin@Artems-MacBook-Pro.local>
2025-12-12 21:17:06 -0800

    Add ORM relationships, association proxies, and cascade rules

--- server/models.py
+++ server/models.py
@@ -3,58 +3,7 @@ from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
-# --- has many Courses ---
-class Instructor(db.Model, SerializerMixin):
-    __tablename__ = 'instructors'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    email = db.Column(db.String)
-
-
-# --- belongs to Instructor, has many Lessons, has many Enrollments  ---
-class Course(db.Model, SerializerMixin):
-    __tablename__ = 'courses'
-
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String)
-    description = db.Column(db.Text)
-
-    instructor_id = db.Column(
-        db.Integer,
-        db.ForeignKey('instructors.id'),
-        nullable=False
-    )
-
-    
-
-# --- belongs to Course ---
-class Lesson(db.Model, SerializerMixin):
-    __tablename__ = 'lessons'
-
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String)
-    content = db.Column(db.Text)
-
-    course_id = db.Column(
-        db.Integer,
-        db.ForeignKey('courses.id'),
-        nullable=False
-    )
-
-   
-
-# --- has many Enrollments ---
-class Student(db.Model, SerializerMixin):
-    __tablename__ = 'students'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    email = db.Column(db.String)
-
-   
-
-# --- belongs to Course and Student
+# --- belongs to Course and belongs to Student ---
 class Enrollment(db.Model, SerializerMixin):
     __tablename__ = 'enrollments'
 
@@ -75,4 +24,87 @@ class Enrollment(db.Model, SerializerMixin):
     progress = db.Column(db.Integer)
     status = db.Column(db.String)
 
-    
+    course = db.relationship("Course", back_populates="enrollments")
+
+    student = db.relationship("Student", back_populates="enrollments")
+
+# --- has many Courses ---
+class Instructor(db.Model, SerializerMixin):
+    __tablename__ = 'instructors'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String)
+
+    courses = db.relationship("Course", back_populates="instructor")
+
+# --- belongs to Instructor, has many Lessons, has many Enrollments  ---
+class Course(db.Model, SerializerMixin):
+    __tablename__ = 'courses'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    description = db.Column(db.Text)
+
+    instructor_id = db.Column(
+        db.Integer,
+        db.ForeignKey('instructors.id'),
+        nullable=False
+    )
+
+    instructor = db.relationship("Instructor", back_populates="courses")
+
+    lessons = db.relationship("Lesson", back_populates="course")
+
+    enrollments = db.relationship(
+        "Enrollment", 
+        back_populates="course",
+        cascade="all, delete-orphan"
+        )
+
+    # --- proxy to get students belongs to the course through Enrollment ---
+    students = association_proxy(
+        "enrollments",
+        "student",
+        # --- add new student to the course ---
+        creator=lambda student_obj: Enrollment(student=student_obj)
+        )
+
+# --- belongs to Course ---
+class Lesson(db.Model, SerializerMixin):
+    __tablename__ = 'lessons'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    content = db.Column(db.Text)
+
+    course_id = db.Column(
+        db.Integer,
+        db.ForeignKey('courses.id'),
+        nullable=False
+    )
+
+    course = db.relationship("Course", back_populates="lessons")
+
+# --- has many Enrollments ---
+class Student(db.Model, SerializerMixin):
+    __tablename__ = 'students'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String)
+
+    enrollments = db.relationship(
+        "Enrollment", 
+        back_populates="student",
+        cascade="all, delete-orphan"
+        )
+
+    # --- proxy to get courses belongs to the student through Enrollment ---
+    courses = association_proxy(
+        "enrollments",
+        "course",
+        # --- add new course to the student ---
+        creator=lambda course_obj: Enrollment(course=course_obj)
+    )
+
